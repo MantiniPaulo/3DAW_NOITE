0f07cc2951617bdd7353aa2d97f9e446fe5e47d9
MantiniPaulo <paulo.1820478300076@faeterj-rio.edu.br>
2021-03-24 16:35:38 -0300

    novo

---
+++ ex01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>3DAW</h1>
+<?php
+$nome1 = "Paulo Mantini";
+$nome2 = "Camilla";
+$nome3 = "Benjamin";
+$nome4 = "Vini";
+$nome5 = "Aline";
+ 
+
+
+
+echo "<h2>Exercicio 1</h2>";
+echo "Alo Alo boaa noite pessoal";
+echo "<table border='1' ><tr><td>$nome1</td><td>$nome2</td><td>$nome3</td></tr>";
+echo "<tr><td>$nome4</td><td>$nome5</td><td>$nome6</td></tr></table>";
+?>
+</body>
+</html>
