3090539466eb48bae26a065f3c8a364a85534fd4
MantiniPaulo <paulo.1820478300076@faeterj-rio.edu.br>
2021-03-29 14:18:42 -0300

    novo29_03

---
+++ PHP_ESTUDOS/Operadores/atribuicao.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = "paulo"; //oeprador de atrubição
+
+echo $nome." roberto <br>"; // operadore de string (concatenação)
+
+$nome .= " Roeberto <br>"; 
+
+echo $nome; 
+
+$valortotal = 0;
+$valortotal += 100;
+$valortotal += 25;
+$valortotal *= .1;
+
+
+echo $valortotal;
+
+
+?>
---
+++ PHP_ESTUDOS/escopoVariaveis.php
@@ -0,0 +1,19 @@
+<?php
+
+$nome = "paulo";
+
+    function teste(){
+        global $nome;
+        echo $nome; 
+    }
+    
+
+function teste2(){
+    $nome = "joao";
+    echo $nome."agora no teste dois";
+    }
+
+teste();
+teste2();
+
+?>
