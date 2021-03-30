0fa2125018a168e52cdb97c04d373a1ef46e2dd2
MantiniPaulo <paulo.1820478300076@faeterj-rio.edu.br>
2021-03-30 10:54:03 -0300

    novo30_03

---
+++ PHP_ESTUDOS/Strings/string1.php
@@ -0,0 +1,15 @@
+<?php
+$nome = "Paulo";
+$nome2 = 'Mantini';
+
+//var_dump($nome, $nome2);
+
+echo "ABC $nome";
+
+echo "<br>";
+
+echo "Ola $nome, tudo bem com você?<br> 
+      $nome, quantos ano você tem?<br>
+      Que empresa $nome, você trabalha";
+
+?>
---
+++ PHP_ESTUDOS/Strings/stringMaiscula.php
@@ -0,0 +1,23 @@
+<?php
+
+$nome = "Paulo Mantini";
+
+$nome = strtoupper($nome); 
+
+echo $nome;
+
+echo "<br>";
+
+$nome = strtolower($nome);
+
+echo $nome;
+
+echo "<br>";
+
+echo ucfirst($nome);
+
+echo "<br>";
+
+echo ucwords($nome);
+
+?>
