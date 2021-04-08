31cca9a260d3f27169fd0cab94e5e67610175837
MantiniPaulo <paulo.1820478300076@faeterj-rio.edu.br>
2021-04-07 22:42:29 -0300

    Exercicio1Calculadora

---
+++ 3DAW_NOITE/Exercicio1_Calculadora/calculadora.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = $_POST["a"]; //recebe o primeiro número do html
+$b = $_POST["b"]; //recebe o segundo número do html
+$operação = $_POST["operacao"]; //recebe a informação da seleção da operação no html
+$resp = 0;
+
+//o if descreve seleciona qual operaçõa fazer
+if ($operação == "soma") {
+    echo $resp = ($a + $b);
+} else if ($operação == "subitração") {
+    echo $resp = ($a - $b);
+} else if ($operação == "multiplicação") {
+    echo $resp = ($a * $b);
+} else {
+    echo $resp = ($a / $b);
+}
+
+?>
